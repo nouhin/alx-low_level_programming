24884ca05888e44b9d5e44751284f1940e473e19
Your Name <you@example.com>
2023-03-19 11:45:23 -0700

    Fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,8 @@
 /**
  * main - Entry point
  *
+ * Desription: print value of n satus; zero, positive or negative
+ *
  * Return: Always 0 (Success)
  */
 
@@ -13,16 +15,16 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if ( n > 0 )
+	
+	if (n > 0)
 	{
-		printf("%d: is positive", n);
-	}else if ( n == 0)
+		printf("%i: is positive\n", n);
+	}else if (n == 0)
 	{
-		printf("%d: is zero", n);
+		printf("%i: is zero\n", n);
 	}else
 	{
-		printf("%d: is negative", n);
+		printf("%i: is negative\n", n);
 	}
 	return (0);
 }
