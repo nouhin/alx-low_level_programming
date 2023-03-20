8da6be0b9b48fb803a9740df74e286dc896fab1a
Nohayla.Ajmi <59204569+nouhin@users.noreply.github.com>
2023-03-20 21:13:48 +0100

    add file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ * Description:
+ *
+ * Return: Always 0 (Succed)
+ */
+int main(void)
+{
+    char c;
+    while (c <= 'z')
+    {
+        putchar(c);
+        c++;
+    }
+}
